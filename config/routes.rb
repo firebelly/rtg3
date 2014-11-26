de98a6c05b59216f16a900c606cbff6362c5b74f
Nate Beaty <nate@firebellydesign.com>
2014-11-26 10:07:30 -0600

    send along params[:id]=‘home’ on homepage to find “Home”

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  root :to => "pages#home"
+  root :to => "pages#home", id: 'home'
 
   resources :reasons, :only => [ :index, :show ]
 
