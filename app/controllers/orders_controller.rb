2d5f6f5a4e8f571355e84533707cc270c59fe91f
Nate Beaty <nate@firebellydesign.com>
2014-12-01 17:38:37 -0600

    oops, missed controller in commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,28 +1,28 @@
 class OrdersController < ApplicationController
 
   def create
-    @result = Braintree::Transaction.sale(
+    result = Braintree::Transaction.sale(
               amount: @cart.total,
               payment_method_nonce: params[:payment_method_nonce])
 
     # todo: store payment_transaction regardless of status?
-    if @result.success?
+    if result.success?
       @order = Order.create(
         cart: @cart,
         first_name: params[:checkoutFirstName],
         last_name: params[:checkoutLastName],
         zip: params[:checkoutLastName],
         email: params[:checkoutEmail],
-        payment_type: @result.transaction.payment_instrument_type,
-        payment_status: @result.transaction.status
+        payment_type: result.transaction.payment_instrument_type,
+        payment_status: result.transaction.status
       )
-      @payment = PaymentRecord.create(
-        order_id: @order.id,
-        params: @result.params
-      )
+      # @payment = PaymentRecord.create(
+      #   order_id: @order.id,
+      #   params: result.params
+      # )
       redirect_to thanks_path(@order)
     else
-      flash[:alert] = "Something went wrong while processing your transaction. Please try again."
+      flash[:alert] = "There was a transaction error: %s" % result.message
       redirect_to :back
     end
   end
