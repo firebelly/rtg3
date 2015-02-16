8f6704afd3d9ab51911bf91d7ec350a0deb88952
Nate Beaty <nate@firebellydesign.com>
2015-02-16 12:25:44 -0600

    update Found Via options, move to array defined in model + rails select_tag, move us_states to array in helper file & use for dropdown, add us_states dropdown to application forms

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -13,6 +13,21 @@ class Donation < ActiveRecord::Base
     end
   end
 
+  def self.found_options
+    [
+      "I've donated before",
+      "I know Firebelly Design",
+      "I am/was a volunteer",
+      "Word of mouth",
+      "Through social media",
+      "A link from another site",
+      "I was searching the web",
+      "At an event I attended",
+      "In the news",
+      "Other"
+    ]
+  end
+
   def found_text
     found == 'other' ? found_other : found
   end
