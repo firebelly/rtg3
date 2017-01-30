c059c9795e805d1507657a4d294f5bd16ebfa45d
Nate Beaty <nate@firebellydesign.com>
2017-01-30 14:32:09 -0600

    send emails from dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,11 +14,22 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_options = {to: ENV['EMAILS_TO'], from: ENV['EMAILS_FROM']}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => 'smtp.webfaction.com',
+      :port                 => 587,
+      :domain               => 'reasontogive.com',
+      :user_name            => ENV['SMTP_USER_NAME'],
+      :password             => ENV['SMTP_PASSWORD'],
+      :authentication       => 'login',
+      :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
